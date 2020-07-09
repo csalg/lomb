aacf1d94a45406abaf26556cb372ef6175866daf
Charlie Salgado <charlie@csalgado.io>
2020-07-09 09:15:55 +0200

    Working on getting the urls to be set from env variables

--- frontend/public/endpoints.js
+++ frontend/public/endpoints.js
@@ -1,4 +1,3 @@
-const API_URL = 'http://localhost:5000'
 const VOCABULARY = API_URL + "/vocabulary"
 const REVISE_URL = VOCABULARY + '/revise'
 const INTERACTION_TRACKING_URL = 'http://localhost:5000/tracking/'
--- frontend/run.sh
+++ frontend/run.sh
@@ -1,3 +1,4 @@
 cd /app
 echo *
+cat 'API_URL=$API_URL' > src/public/api_endpoint.js
 npm start
