df1e872d7a6e71bd731068a0d60f4afd8d010292
Charlie Salgado <charlie@csalgado.io>
2020-07-11 12:53:12 +0200

    Fixed tracking to hardcoded url

--- frontend/public/endpoints.js
+++ frontend/public/endpoints.js
@@ -1,3 +1,3 @@
 const VOCABULARY = API_URL + "/vocabulary"
 const REVISE_URL = VOCABULARY + '/revise'
-const INTERACTION_TRACKING_URL = 'http://localhost:5000/tracking/'
+const INTERACTION_TRACKING_URL = API_URL+'/tracking'
