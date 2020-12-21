3e84e1fd47e4dabd96144064d10adf5692b39c6c
Charlie Salgado <charlie@csalgado.io>
2020-12-21 12:08:40 +0100

    Fixed Leitner and added some tests

--- backend/src/services/vocabulary/infrastructure/ml.py
+++ backend/src/services/vocabulary/infrastructure/ml.py
@@ -1,7 +1,8 @@
-
+limit = lambda n, minn, maxn: max(min(maxn, n), minn)
 
 def probability_of_recall_leitner(elapsed, successes, failures):
     delta = elapsed/(60*60)
-    h = 2.0 ** (successes - failures)
+    success_failure_difference = limit(successes - failures, -20, 20)
+    h = 2.0 ** success_failure_difference
     p = 2.0 ** (-(delta / h))
     return p
