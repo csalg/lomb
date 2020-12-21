46ad891c4452277d4fdcab134de53e5c12a43ab0
Charlie Salgado <charlie@csalgado.io>
2020-12-21 11:54:00 +0100

    Simplified Leitner algo more

--- backend/src/services/vocabulary/infrastructure/ml.py
+++ backend/src/services/vocabulary/infrastructure/ml.py
@@ -1,7 +1,7 @@
 
 
 def probability_of_recall_leitner(elapsed, successes, failures):
-    delta_hours = elapsed/(60*60)
+    delta = elapsed/(60*60)
     h = 2.0 ** (successes - failures)
     p = 2.0 ** (-(delta / h))
     return p
