12b0b6a4489f6b86e15bd0eecd9112b74732a6cd
Carlos Salgado <charlie@csalgado.io>
2021-02-24 23:26:16 +0100

    Deleted unused event handler in tracking

--- backend/src/services/tracking/event_handlers.py
+++
@@ -1,8 +0,0 @@
-from flask import current_app
-
-
-def stop_learning_lemma_handler(controllers):
-    def inner(stop_learning_lemma_event):
-        current_app.logger.info('Inside tracking')
-        controllers.ignore_lemma(stop_learning_lemma_event.username, stop_learning_lemma_event.lemma, stop_learning_lemma_event.source_language)
-    return inner
