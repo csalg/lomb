8c34d44cbc1529e77db0307f77914695daf6d59d
Carlos Salgado <charlie@csalgado.io>
2021-04-25 17:59:28 +0200

    Return frequency filtering because performance

--- backend/src/services/vocabulary/controllers.py
+++ backend/src/services/vocabulary/controllers.py
@@ -107,7 +107,7 @@ class Controllers:
         # return result
 
     def learning_lemmas_with_probability_smart_fetch(self, query: ReviseQueryDTO):
-        lemmas = self.learning_lemmas(query.username, 0)
+        lemmas = self.learning_lemmas(query.username, query.minimum_frequency)
         lemmas.sort(key=calculate_lemma_frequency, reverse=True)
         result = []
         for lemma in lemmas:
