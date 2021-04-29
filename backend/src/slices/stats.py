5fe1db6531adbd8f3025f23116fd1f98e14e8785
Carlos Salgado <charlie@csalgado.io>
2021-04-29 21:16:28 +0200

    Add learning lemmas count

--- backend/src/slices/stats.py
+++ backend/src/slices/stats.py
@@ -1,15 +1,11 @@
 import time
 
-from config import VOCABULARY_LOGS_COLLECTION_NAME, IGNORE_LEMMAS_COLLECTION_NAME
+from config import VOCABULARY_LOGS_COLLECTION_NAME, IGNORE_LEMMAS_COLLECTION_NAME, LEARNING_LEMMAS_COLLECTION_NAME
 from lib.db import get_db
 
 db = get_db()
 
 def stats(username):
-    # Find all logs for user
-    # Keep maps for seen last 24h, week, month
-    # Calculate PoR
-    # Find count of ignore list
     ignored_lemmas = db[IGNORE_LEMMAS_COLLECTION_NAME]\
                         .find({'user': username})\
                         .count()
@@ -19,12 +15,17 @@ def stats(username):
         .find({'user': username, 'timestamp': {"$gte": when}}) \
         .count()
 
+    learning_lemmas = db[LEARNING_LEMMAS_COLLECTION_NAME]\
+                        .find({'user': username})\
+                        .count()
 
     a_day_ago = current_timestamp - 24*60*60
     a_week_ago = current_timestamp - 7*24*60*60
     a_month_ago = current_timestamp - 30*24*60*60
+
     return {
         "ignored_lemmas": ignored_lemmas,
+        "learning_lemmas": learning_lemmas,
         "lemmas_with_high_por": 2130,
         "seen_last_day": seen_last(a_day_ago),
         "seen_last_week": seen_last(a_week_ago),
--- frontend/src/scenes/UserArea/Stats/index.jsx
+++ frontend/src/scenes/UserArea/Stats/index.jsx
@@ -1,48 +1,43 @@
 import React from 'react';
-import AuthService from "../../../services/auth";
 import {STATS_URL} from "../../../endpoints";
-import awaitAuthService from "../../../services/auth";
+import AuthService from "../../../services/auth";
 
 export default class extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             loading: true,
-            ignored_lemmas: 147,
-            lemmas_with_high_por: 230,
-            seen_last_day: 10,
-            seen_last_week: 20,
-            seen_last_month: 30,
-            seen_total: 40,
         }
 
         this.fetchStats = this.fetchStats.bind(this)
     }
-
 
     componentDidMount() {
         this.fetchStats()
     }
 
     async fetchStats(){
-        const res = await awaitAuthService.jwt_get(STATS_URL)
-        console.log(res)
-        this.setState({...(res.data), loading: false})
+        const res = await AuthService.jwt_get(STATS_URL)
+        await this.setState({...(res.data), loading: false})
     }
 
     render() {
+        const formatAmounts = num => num ? num.toLocaleString('en-US', {maximumFractionDigits:0}) : 0
+        const weeklyAverage = formatAmounts(this.state.seen_last_week/7)
+        const monthlyAverage = formatAmounts(this.state.seen_last_month/30)
         return (<div style={{padding: '2em 0'}}>
             <span style={{display: this.state.loading? 'auto' : 'none'}}>Loading</span>
             <ul style={{display: this.state.loading? 'none': 'block'}}>
-                <li>There are {this.state.ignored_lemmas} lemmas in your ignore list.</li>
+                <li>There are {formatAmounts(this.state.ignored_lemmas)} lemmas in your ignore list.</li>
+                <li>You have added {formatAmounts(this.state.learning_lemmas)} lemmas to your learning list.</li>
                 {/*<li>Of the lemmas you are learning, {this.state.lemmas_with_high_por} have a probability of recall (PoR)*/}
                 {/*    of 0.5 or higher*/}
                 {/*</li>*/}
                 <li>Activity level - here's how many exposures you have logged recently:</li>
                 <ul>
-                    <li>{this.state.seen_last_day} in the past 24h.</li>
-                    <li>{this.state.seen_last_week} in the past week, </li>
-                    <li>{this.state.seen_last_month} in the past month.</li>
+                    <li>{formatAmounts(this.state.seen_last_day)} in the past 24h.</li>
+                    <li>{formatAmounts(this.state.seen_last_week)} in the past week ({weeklyAverage} avg/day).</li>
+                    <li>{formatAmounts(this.state.seen_last_month)} in the past month ({monthlyAverage} avg/day).</li>
                 </ul>
                 {/*<li>Distribution of learning words and PoR Over a certain frequency?</li>*/}
             </ul>
