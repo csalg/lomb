7fdc507755a63bdb9997a9cc1f426b49152adb3d
Carlos Salgado <charlie@csalgado.io>
2021-05-16 21:59:06 +0200

    Add keras model

--- backend/src/slices/probabilities/__init__.py
+++ backend/src/slices/probabilities/__init__.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from flask import current_app
 from sklearn.linear_model import LinearRegression
+import tensorflow.keras as keras
 import pickle
 
 from config import DATAPOINTS, MAX_ELAPSED
@@ -19,8 +20,9 @@ class MTR:
         self.column_names = []
 
     def load(self):
-        with open('src/slices/probabilities/model.pkl', 'rb') as file:
-            self.model = pickle.load(file)
+        # with open('src/slices/probabilities/model.pkl', 'rb') as file:
+        #     self.model = pickle.load(file)
+        self.model = keras.models.load_model('src/slices/probabilities/keras_model')
 
         with open('src/slices/probabilities/column_names.pkl', 'rb') as file:
             self.column_names = pickle.load(file)
@@ -29,9 +31,12 @@ class MTR:
         X = df[self.column_names]
         X = engineer_features(X)
         now = int(time.time())
-        mu_pred = self.model.predict(X)
+
+        mu_pred_2d = np.clip(self.model.predict(X), MU_MIN, MU_MAX)
+        mu_pred = mu_pred_2d.reshape(-1)
         delta = now - df['timestamp']
         df['score_pred'] = SimplifiedWickelgren.calculate_retention_rate(mu_pred, delta)
+
         return df
 
 
@@ -55,19 +60,50 @@ class SimplifiedWickelgren:
         return "SWP"
 
 
+class HLRSqrt:
+
+    @staticmethod
+    def calculate_mu(retention_rate, delta):
+        delta_sqrt = np.sqrt(delta)
+        retention_rate_clipped = np.clip(retention_rate, 0.001, 0.999)
+        term = (-1 * delta_sqrt) / np.log2(retention_rate_clipped)
+        mu = np.log2(term)
+        return np.clip(mu, MU_MIN + 5, MU_MAX)
+
+    @staticmethod
+    def calculate_retention_rate(mu, delta):
+        delta_sqrt = np.sqrt(delta)
+        mu = np.clip(mu - 5, MU_MIN, MU_MAX)
+        exponent = np.divide(-1 * delta_sqrt, np.power(2., mu))
+        return np.power(2., exponent)
+
+    @staticmethod
+    def get_name():
+        return "HLR"
+
+
 def engineer_features(X):
     for feature_name in X.columns:
         if 'seconds' in feature_name:
-            mask = X[feature_name].where(X[feature_name]<MAX_ELAPSED)
+            mask = X[feature_name].where((0 < X[feature_name]) & (X[feature_name] < MAX_ELAPSED))
+
+            sqrt = np.sqrt(mask)
+            X[feature_name + "_sqrt"] = np.nan_to_num(sqrt)
+
             inverse = np.clip(np.divide(1.0, mask), 10e-10, 10e-6)
-            log_inverse = np.log(inverse)
-            X[feature_name+"_inverse"] = np.nan_to_num(inverse)
-            X[feature_name+"_log_inverse"] = np.nan_to_num(log_inverse)
+            X[feature_name + "_inverse"] = np.nan_to_num(inverse)
+
+            log_inverse = np.sqrt(inverse)
+            X[feature_name + "_sqrt_inverse"] = np.nan_to_num(log_inverse)
+
             if 'FIRST_EXPOSURE' in feature_name:
                 continue
+
             X.drop(feature_name, axis=1, inplace=True)
         if 'amount' in feature_name:
-            X[feature_name+"_sqrt"] = np.sqrt(X[feature_name])
+            X[feature_name + "_sqrt"] = np.sqrt(X[feature_name])
+            X.drop(feature_name, axis=1, inplace=True)
+
     return X
 
 
