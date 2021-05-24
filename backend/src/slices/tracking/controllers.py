670ce673b80a9421965a1c9d8c246941b4e657a5
Carlos Salgado <charlie@csalgado.io>
2021-05-24 23:47:50 +0200

    Performance profile etl

--- backend/src/slices/tracking/controllers.py
+++ backend/src/slices/tracking/controllers.py
@@ -18,12 +18,14 @@ class Controllers:
 
     def ignore_lemma(self, user, lemma, source_language):
         current_app.logger.info(f"Ignoring {user, lemma, source_language}")
+        start = int(time.time()*1000)
         timestamp = int(time.time()*1000)
         self.__learning_repository.delete(user, lemma, source_language)
         current_app.logger.info(f'Deleting from learning collection took {(int(time.time()*1000))-timestamp}')
         timestamp = int(time.time()*1000)
         self.__ignore_repository.add(user, lemma, source_language)
-        current_app.logger.info(f'Adding to ignore {(int(time.time()*1000))-timestamp}')
+        current_app.logger.info(f'Adding to ignore collection took {(int(time.time()*1000))-timestamp}')
+        current_app.logger.info(f'Ignoring took {(int(time.time()*1000))-start}')
 
     def add(self, user, message, lemmas, source_language, support_language):
         if message not in VALID_MESSAGES:
